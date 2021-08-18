ee4c953efce2fd43792d5c4a2a3b20ec8e96740a
ankur <ankur.sh2011@gmail.com>
2021-08-18 10:05:14 +0530

    final commit not solving the query but get data from database

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,15 +1,23 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
 import { CreateLessonInput } from './lesson.input';
-// import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
+import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
 import { Lesson } from './lesson.entity';
-// import { StudentService } from '../student/student.service';
+import { StudentService } from '../student/student.service';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
   constructor(
-    private lessonService: LessonService, // private studentService: StudentService,
+    private lessonService: LessonService,
+    private studentService: StudentService,
   ) {}
 
   @Query((returns) => LessonType)
@@ -29,17 +37,23 @@ export class LessonResolver {
     return this.lessonService.createLesson(createLessonInput);
   }
 
-  // @Mutation((returns) => LessonType)
-  // assignStudentsToLesson(
-  //   @Args('assignStudentsToLessonInput')
-  //   assignStudentsToLessonInput: AssignStudentsToLessonInput,
-  // ) {
-  //   const { lessonId, studentIds } = assignStudentsToLessonInput;
-  //   return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
-  // }
+  @Mutation((returns) => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
 
   // @ResolveField()
   // async students(@Parent() lesson: Lesson) {
   //   return this.studentService.getManyStudents(lesson.students);
   // }
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    // console.log(lesson);
+    return this.studentService.getManyStudents(lesson.students);
+  }
 }
--- src/lesson/lesson.type.ts
+++ src/lesson/lesson.type.ts
@@ -1,5 +1,5 @@
 import { ObjectType, Field, ID } from '@nestjs/graphql';
-// import { StudentType } from 'src/student/student.type';
+import { StudentType } from '../student/student.type';
 
 @ObjectType('Lesson')
 export class LessonType {
@@ -15,6 +15,6 @@ export class LessonType {
   @Field()
   endDate: string;
 
-  //   @Field((type) => [StudentType])
-  //   students: string[];
+  @Field((type) => [StudentType])
+  students: string[];
 }
